7ef0b0cdbf199e768573df667c46aabc72c4d41f
george <george.tutuianu@hotmail.com>
2019-11-25 15:15:21 +0200

    added logging for debugging

--- ScrapyKeeper/app/proxy/contrib/scrapy.py
+++ ScrapyKeeper/app/proxy/contrib/scrapy.py
@@ -2,6 +2,7 @@ import datetime, time
 
 import requests
 
+from ScrapyKeeper.app import app
 from ScrapyKeeper.app.proxy.spiderctrl import SpiderServiceProxy
 from ScrapyKeeper.app.spider.model import SpiderStatus, Project, SpiderInstance
 from ScrapyKeeper.app.util.http import request
@@ -78,6 +79,7 @@ class ScrapydProxy(SpiderServiceProxy):
         post_data = dict(project=project_name, spider=spider_name)
         post_data.update(arguments)
         data = request("post", self._scrapyd_url() + "/schedule.json", data=post_data, return_type="json")
+        app.logger.warn("The response from {} was {}".format(self._scrapyd_url(), data['status'] if data else data.status_code))
         if data and data['status'] == 'ok':
             return data['jobid']
         else:
